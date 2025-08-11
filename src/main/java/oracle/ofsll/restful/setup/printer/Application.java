69c6c83d1693671172a58218e3a279761afe668e
prashantsingh124 <prashantsingh12012@gmail.com>
2025-08-11 16:32:13 +0530

    Bug fixes #1213

--- src/main/java/oracle/ofsll/restful/setup/printer/Application.java
+++ src/main/java/oracle/ofsll/restful/setup/printer/Application.java
@@ -2,7 +2,7 @@ package oracle.ofsll.restful.setup.printer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//added changers again in the code
+//added changers again in the code ---agains
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
